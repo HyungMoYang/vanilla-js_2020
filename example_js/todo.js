34ef4a11422883a14eea27e308a8ce968802b5b5
HyungMoYang <42955111+HyungMoYang@users.noreply.github.com>
2020-05-11 22:57:17 +0900

    update To-do List delete

--- example_js/todo.js
+++ example_js/todo.js
@@ -4,7 +4,21 @@ const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos"; // localStorage의 현재 상태를 위한
-const toDos = []; // To-do List를 저장하는 Array
+let toDos = []; // To-do List를 저장하는 Array
+
+// To-do List를 삭제하는 함수
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function (toDo) {
+    console.log(toDo.id, li.id);
+    console.log(toDo.id, parseInt(li.id));
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos; // 바뀐 Array를 새로 저장 toDos가 변화하므로 let으로 바꿔야함.
+  saveToDos(); // 바뀐 Array를 localStorage에 저장
+}
 
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -16,6 +30,7 @@ function paintToDo(text) {
   const span = document.createElement("span");
   const newId = toDos.length + 1;
   delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
   li.appendChild(span);
   li.appendChild(delBtn);
